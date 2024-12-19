b0fd08b8eebe391c2fdab9dff79b5ffeb7c94c63
bolshakov863 <ivan.bolshakov113@gmail.com>
2024-12-19 16:20:32 +0300

    create a new repository

---
+++ menu.py
@@ -0,0 +1,63 @@
+from tkinter import *
+from pickle import load, dump
+
+def menu_enter():
+    pass
+
+
+
+def game_new():
+    pass
+
+
+
+
+
+
+def game_resume():
+    pass
+
+
+
+
+def game_save():
+    pass
+
+
+def game_load():
+    pass
+
+
+def game_exit():
+    pass
+
+
+def menu_show():
+    pass
+
+def menu_hide():
+    pass
+
+
+def menu_up():
+    pass
+
+
+def menu_down():
+    pass
+
+
+def menu_update():
+    pass
+
+
+def menu_create(canvas):
+    pass
+
+menu_mode = True
+menu_options = ['Возврат в игру', 'Новая игра', 'Сохранить', 'Загрузить', 'Выход']
+menu_current_index = 3
+menu_options_id = []
+
+game_over = False
+pause = False
